1f11513b8ee15700af45930d7b21b15ddf11390b
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-04-25 14:26:31 -0500

    ..

---
+++ parseSort.cpp
@@ -0,0 +1,14 @@
+#include "parseSort.hpp"
+//22
+parseSort::parseSort(std::vector<std::string> param){
+unParsedLog = param;
+};
+void parseSort::filter(){
+    std::string a;
+    std::stringstream ss(a);
+    for(int i = 0; i < unParsedLog.size(); i++){
+        
+        }
+  
+
+};
--- parseSort.hpp
+++ parseSort.hpp
@@ -1,20 +1,25 @@
-#include <vector>
 #include <string>
-class parseSort{
+#include <vector>
+#include <sstream>
+class parseSort {
 
 public:
-parseSort(std::vector<std::string> param);
-std::vector<std::string> resourses;
-std::vector<std::string> engine;
-std::vector<std::string> init;
-std::vector<std::string> gui;
-std::vector<std::string> profiling;
-std::vector<std::string> script;
-std::vector<std::string> backend;
-std::vector<std::string> def;
-std::vector<std::string> world;
-std::vector<std::string> render;
-std::vector<std::string> network;
-std::vector<std::string> platform;
+  parseSort(std::vector<std::string> param);
+  void filter() {};
+  std::vector<std::string> unParsedLog;
 
+  // categories
+  std::vector<std::string> resourses;
+  std::vector<std::string> engine;
+  std::vector<std::string> init;
+  std::vector<std::string> gui;
+  std::vector<std::string> profiling;
+  std::vector<std::string> script;
+  std::vector<std::string> backend;
+  std::vector<std::string> def;
+  std::vector<std::string> world;
+  std::vector<std::string> render;
+  std::vector<std::string> network;
+  std::vector<std::string> platform;
+  // TODO: monitoring
 };

221bc45bf1f6e4465341d8f1398adad6703ce151
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-04-25 13:46:24 -0500

    init category

--- parseSort.hpp
+++ parseSort.hpp
@@ -1,6 +1,20 @@
+#include <vector>
+#include <string>
 class parseSort{
 
 public:
-parseSort();
+parseSort(std::vector<std::string> param);
+std::vector<std::string> resourses;
+std::vector<std::string> engine;
+std::vector<std::string> init;
+std::vector<std::string> gui;
+std::vector<std::string> profiling;
+std::vector<std::string> script;
+std::vector<std::string> backend;
+std::vector<std::string> def;
+std::vector<std::string> world;
+std::vector<std::string> render;
+std::vector<std::string> network;
+std::vector<std::string> platform;
 
 };

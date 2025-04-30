db5ade28b96477ab36d82f98ba609e2b218f9535
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-04-30 11:45:18 -0500

    setDir

--- monitoring.cpp
+++ monitoring.cpp
@@ -5,5 +5,17 @@ void monitoring::watcher(){
 }
 void monitoring::pickFile(){
 
+
+}
+void monitoring::setDirectory(std::string PATH){
+    fileDirectory = PATH;
+    try{d.assign(fileDirectory);
+        if(d.path() != fileDirectory) throw 201;
     
+    }catch(...){
+        std::cout << "DIRECTORY ERROR\n";
+    };
+}
+std::string monitoring::getDirectory(){
+    return fileDirectory;
 }
--- monitoring.hpp
+++ monitoring.hpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 class monitoring{
 public:
 void watcher();
 void pickFile();
+void setDirectory(std::string PATH);
+std::string getDirectory();
 private:
-
+std::string fileDirectory;
+std::filesystem::directory_entry d;
 
 
 };

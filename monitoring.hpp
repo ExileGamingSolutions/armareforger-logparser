82e8438523b58e8dfebcabf01c09d8a75cef6699
= <=>
2025-04-30 17:26:51 -0500

    fileselector

--- monitoring.hpp
+++ monitoring.hpp
@@ -1,17 +1,21 @@
-#include <iostream>
-#include <fstream>
 #include <filesystem>
+#include <fstream>
+#include <iostream>
 
-class monitoring{
+class monitoring {
 public:
-void watcher();
-void fileList();
-void setDirectory(std::string PATH);
-std::string getDirectory();
+  void watcher();
+  void fileList();
+  void setDirectory(std::string PATH);
+  std::filesystem::path selectFile(std::vector<std::filesystem::path> dirContents);
+  std::string getDirectory();
+  std::string getPath();
+  std::vector<std::filesystem::path> getDirContents();
 private:
-std::string _fileDirectory;
-std::filesystem::directory_entry _d;
-std::vector<std::filesystem::path> _dirContents;
-
-
+  std::filesystem::path _compareFileDate(std::filesystem::path path1,
+                                         std::filesystem::path path2);
+  std::string _fileDirectory;
+  std::filesystem::directory_entry _d;
+  std::vector<std::filesystem::path> _dirContents;
+  std::filesystem::path currentPath;
 };

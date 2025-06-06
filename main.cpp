a394811c5c8078a461aa8363ea665a35bc171b00
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-06-06 17:41:36 -0500

    restruct

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.hpp"
-
-int main(){
- _scan.readFile("test.txt");
-unParsedLogs = _scan.logLines;
-_parseSort.filter(unParsedLogs);
-for(int i =0; i < _parseSort.init.size(); i++){
-std::cout << _parseSort.init[i] << std::endl;}
-}
--- main.hpp
+++
@@ -1,6 +0,0 @@
-#include "parseSort.cpp"
-#include "scan.cpp"
-
-scan _scan;
-parseSort _parseSort;
-std::vector<std::string> unParsedLogs;

5eb4b3a74be6196c426f0b9b6f4bf9ab92947796
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-06-10 17:14:28 -0500

    setup test

--- src/include/main.hpp
+++ src/include/main.hpp
@@ -1,6 +1,8 @@
 #include "sub/parseSort.hpp"
 #include "sub/scan.hpp"
+#include "sub/sock.hpp"
 
 scan _scan;
 parseSort _parseSort;
 std::vector<std::string> unParsedLogs;
+sock _sock;
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,10 +1,12 @@
 #include "main.hpp"
 
 int main() {
-  _scan.readFile("test.txt");
-  unParsedLogs = _scan.logLines;
-  _parseSort.filter(unParsedLogs);
-  for (int i = 0; i < _parseSort.init.size(); i++) {
-    std::cout << _parseSort.init[i] << std::endl;
-  }
+  // _scan.readFile("test.txt");
+  // unParsedLogs = _scan.logLines;
+  //_parseSort.filter(unParsedLogs);
+
+  _sock.setMessage("test");
+  _sock.connectToServer();
+  _sock.sendToServer();
+  _sock.closeServerConnect();
 }

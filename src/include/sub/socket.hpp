bbe257cc95a4e90cbc4dbd3261d8e311c89e933c
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-06-10 16:26:49 -0500

    socket.h def

---
+++ src/include/sub/socket.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+class sock {
+
+  int link = socket(AF_INET, SOCK_STREAM, 0);
+
+  sockaddr_in server;
+
+  char *message;
+  void setMessage(std::string message);
+};

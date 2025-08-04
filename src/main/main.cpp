87fcc7002566bbe7567e639f2c8e89d1d12d0718
Zachary Taylor <zacharytaylor0508@gmail.com>
2025-08-04 10:28:28 -0500

    comment currently uneeded

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,24 +11,26 @@ int main() {
 
   std::cout << "SIZE IS: " << sizeof(message) << std::endl;
   std::cout << "SIZE 2 IS: " << sizeof(_parseSort.init[0]) << std::endl;
-  switch (int i = 0) {
-  case 0:
-    if (_sock.connectToServer()) {
-      std::cout << "CONNECTED\n";
-      i++;
-    } else {
-      i = 0;
-      std::cout << "FAILED TO CONNECT, RETRYING\n";
-    }
 
-  case 1:
-    if (_sock.sendClumpToServer(_parseSort.world)) {
-      std::cout << "SENT \n";
-      i++;
-    }
-  case 2:
-    _sock.closeServerConnect();
-  }
+  /*
+    switch (int i = 0) {
+    case 0:
+      if (_sock.connectToServer()) {
+        std::cout << "CONNECTED\n";
+        i++;
+      } else {
+        i = 0;
+        std::cout << "FAILED TO CONNECT, RETRYING\n";
+      }
+
+    case 1:
+      if (_sock.sendClumpToServer(_parseSort.world)) {
+        std::cout << "SENT \n";
+        i++;
+      }
+    case 2:
+      _sock.closeServerConnect();
+    }*/
   //  int x = _sock.connectToServer();
   //  int m = _sock.sendToServer();
 
